522e18e555c1431518923ee3b0c2d486f90c3137
Bougado <bougadowilliam@gmail.com>
2024-10-01 01:15:16 +0200

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-portfolio-project",
+  base: "/my-portfolio-project/",
   plugins: [react()],
 });
