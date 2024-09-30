03ed82062ab8eea23f9667c10c10fd91d118649f
Bougado <bougadowilliam@gmail.com>
2024-10-01 01:30:05 +0200

    revert changes + tempo comment textAnimation

--- src/components/TextAnimation.jsx
+++ src/components/TextAnimation.jsx
@@ -2,39 +2,46 @@ import React, { useEffect, useState } from "react";
 import SplitType from "split-type";
 import { gsap } from "gsap";
 
-const TextAnimation = ({ onAnimationComplete }) => {
-  const [visible, setVisible] = useState(true);
+const TextAnimation = () => {
+  const [visible, setVisible] = useState(true); // État pour gérer la visibilité
 
   useEffect(() => {
-    const myText = new SplitType("#my-text");
+    // Initialiser SplitType
+    // const myText = new SplitType("#my-text");
+
+    // Sélectionner la section intro-slider
     const introSection = document.querySelector("#intro-slider");
 
+    // Commencer par rendre la section visible
     gsap.set(introSection, { opacity: 1 });
 
+    // Animer avec GSAP
     gsap.fromTo(
       ".char",
-      { y: 115, opacity: 0 },
+      {
+        y: 115, // Commence à partir de 115px vers le bas
+        opacity: 0,
+      },
       {
         y: 0,
         opacity: 1,
         stagger: 0.05,
         duration: 0.5,
         onComplete: () => {
+          // Animation de sortie après un léger délai
           gsap.to(".char", {
-            y: -115,
+            y: -115, // Sort vers le haut
             opacity: 0,
             stagger: 0.05,
             duration: 0.5,
             delay: 1,
             ease: "power2.inOut",
             onComplete: () => {
+              // Faire disparaître le fond avec un fade out
               gsap.to(introSection, {
                 opacity: 0,
                 duration: 0.5,
-                onComplete: () => {
-                  setVisible(false);
-                  onAnimationComplete(); // Appeler la fonction de rappel
-                },
+                onComplete: () => setVisible(false), // Masquer le composant après l'animation
               });
             },
           });
@@ -42,11 +49,13 @@ const TextAnimation = ({ onAnimationComplete }) => {
       }
     );
 
+    // Nettoyage à la fin
     return () => {
-      myText.revert();
+      myText.revert(); // Revenir à l'état d'origine
     };
-  }, [onAnimationComplete]);
+  }, []);
 
+  // Rendre conditionnellement le composant
   return visible ? (
     <section id="intro-slider" className="intro-section">
       <h1 id="my-text">Bougado Gaboudo</h1>
