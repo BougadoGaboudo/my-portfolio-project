8fad89f333bf93a104eb9a85d046cfeadf34ac6a
Bougado <bougadowilliam@gmail.com>
2024-10-13 17:15:33 +0200

    feat(about): update about responsive design

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Presentation = () => {
-    return (
-        <section class="presentation-section">
-        <div class="about hiddenY">
-            <img class="logo-size" src="img/LogoRond.png" alt=""/>
-            <div class="txt-about">
-                <h4>About</h4>
-                <h1>Bougado</h1>
-                <p>
-                    I studied illustration and specialize in character art and character design for games, light novels and illustration books.
-                    I'm also working on web projects as a student in BTS SIO SLAM.
-                </p>
-            </div>
+  return (
+    <section class="presentation-section">
+      <div class="presentation-wrapper">
+        <img class="presentation-logo" src="img/LogoRond.png" alt="Logo" />
+        <div class="presentation-txt">
+          <h4>About</h4>
+          <h1>Bougado</h1>
+          <p>
+            I studied illustration and specialize in character art and character
+            design for games, light novels and illustration books. I'm also
+            working on web projects as a student in BTS SIO SLAM.
+          </p>
         </div>
+      </div>
     </section>
-    );
+  );
 };
 
 export default Presentation;
