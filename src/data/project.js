a41e842a21c0863aa939e386fd94fc9bea06f13a
Bougado <bougadowilliam@gmail.com>
2024-10-13 17:59:24 +0200

    feat(project): update stack data

--- src/data/project.js
+++ src/data/project.js
@@ -1,59 +1,66 @@
 export const myProjects = [
-    {
-      title: "Mon premier site web",
-      img: "/img/perso.JPG",
-      description: "Mon tout premier site réalisé dans le cadre de mon BTS SIO SLAM.",
-      tech:"HTML - CSS",
-      url:"https://bougadogaboudo.github.io/Mon-premier-site/"
-    },
-    {
-      title: "Arcane - Accessibilité",
-      img: "/img/arcane.JPG",
-      description:"Mon deuxième site réalisé dans le cadre de mon BTS SIO SLAM avec pour contexte l'accessibilité numérique.",
-      tech:"HTML - CSS - JavaScript",
-      url:"https://bougadogaboudo.github.io/SiteAccess/"
-    },
-    {
-      title: "PokeDaily - MVC",
-      img: "/img/pokedaily.JPG",
-      description:"Mon troisième site réalisé dans le cadre de mon BTS SIO SLAM avec pour contexte la structure MVC.",
-      tech:"HTML - CSS - JavaScript - PHP - MySQL",
-      url:"https://github.com/BougadoGaboudo/SiteMVC"
-    },
-    {
-      title: "React Flag",
-      img: "/img/flag.jpg",
-      description:"Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des drapeaux de pays.",
-      tech:"React - SCSS",
-      url:"https://bougado-1er-cours-react-prod.vercel.app/"
-    },
-    {
-      title: "React Cook",
-      img: "/img/cook.jpg",
-      description:"Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des recettes de cuisine.",
-      tech:"React - SCSS",
-      url:"https://bougado-react-cook-dev.vercel.app/"
-    },
-    {
-      title: "Crypto Watch",
-      img: "/img/cryptowatch.jpg",
-      description:"Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des cryptomonnaies / Utilisation de Recharts et Redux.",
-      tech:"React - SCSS",
-      url:"https://bougado-react-crypto-watch-dev.vercel.app/"
-    },
-    {
-      title: "Site vitrine FS Agency",
-      img: "/img/vitrine.jpg",
-      description:"Projet web réalisé à partir de la formation du youtuber From Scratch - Création de petites animations JS.",
-      tech:"React - SCSS",
-      url:"https://bougado-react-site-vitrine-dev.vercel.app/"
-    },
-    {
-      title: "PointerJobs",
-      img: "/img/pointerjobs.jpg",
-      description:"Participation au développement de l'application web PointerJobs chez Lambda-Labs pendant mon stage de 1ère année en BTS SIO SLAM.",
-      tech:"Next.js - SCSS - Prisma - Jira",
-      url:"https://www.pointer-jobs.com/"
-    },
-  ];
-  
+  {
+    title: "Mon premier site web",
+    img: "/img/perso.JPG",
+    description:
+      "Mon tout premier site réalisé dans le cadre de mon BTS SIO SLAM.",
+    tech: "HTML - CSS",
+    url: "https://bougadogaboudo.github.io/Mon-premier-site/",
+  },
+  {
+    title: "Arcane - Accessibilité",
+    img: "/img/arcane.JPG",
+    description:
+      "Mon deuxième site réalisé dans le cadre de mon BTS SIO SLAM avec pour contexte l'accessibilité numérique.",
+    tech: "HTML - CSS - JavaScript - GitHub",
+    url: "https://bougadogaboudo.github.io/SiteAccess/",
+  },
+  {
+    title: "PokeDaily - MVC",
+    img: "/img/pokedaily.JPG",
+    description:
+      "Mon troisième site réalisé dans le cadre de mon BTS SIO SLAM avec pour contexte la structure MVC.",
+    tech: "HTML - CSS - JavaScript - PHP - MySQL - GitHub",
+    url: "https://github.com/BougadoGaboudo/SiteMVC",
+  },
+  {
+    title: "React Flag",
+    img: "/img/flag.jpg",
+    description:
+      "Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des drapeaux de pays.",
+    tech: "React - SCSS",
+    url: "https://bougado-1er-cours-react-prod.vercel.app/",
+  },
+  {
+    title: "React Cook",
+    img: "/img/cook.jpg",
+    description:
+      "Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des recettes de cuisine.",
+    tech: "React - SCSS",
+    url: "https://bougado-react-cook-dev.vercel.app/",
+  },
+  {
+    title: "Crypto Watch",
+    img: "/img/cryptowatch.jpg",
+    description:
+      "Projet web réalisé à partir de la formation du youtuber From Scratch - Appel d'API pour des cryptomonnaies / Utilisation de Recharts et Redux.",
+    tech: "React - SCSS",
+    url: "https://bougado-react-crypto-watch-dev.vercel.app/",
+  },
+  {
+    title: "Site vitrine FS Agency",
+    img: "/img/vitrine.jpg",
+    description:
+      "Projet web réalisé à partir de la formation du youtuber From Scratch - Création de petites animations JS.",
+    tech: "React - SCSS",
+    url: "https://bougado-react-site-vitrine-dev.vercel.app/",
+  },
+  {
+    title: "PointerJobs",
+    img: "/img/pointerjobs.jpg",
+    description:
+      "Participation au développement de l'application web PointerJobs chez Lambda-Labs pendant mon stage de 1ère année en BTS SIO SLAM.",
+    tech: "Next.js - SCSS - Prisma - GitHub - Jira",
+    url: "https://www.pointer-jobs.com/",
+  },
+];
